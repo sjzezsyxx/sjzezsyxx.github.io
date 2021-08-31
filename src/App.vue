41b0cae149cd427539972a19db854ad9f8044eff
Ren Baoshuo <i@baoshuo.ren>
2021-08-31 18:40:22 +0800

    feat(src/components/Footer.vue): add footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
-    <Header />
-    <div class="container mx-auto px-4 max-w-screen-xl">
-        <Main />
-    </div>
+    <main class="flex flex-col justify-between">
+        <div>
+            <Header />
+            <div class="container mx-auto px-4 max-w-screen-xl">
+                <Main />
+            </div>
+        </div>
+        <Footer />
+    </main>
 </template>
 
 <script setup>
 import Header from './components/Header.vue';
 import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
 </script>
+
+<style scoped>
+main {
+    min-height: 100vh;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div
+        class="
+            bottom-0
+            z-40
+            lg:z-50
+            w-full
+            mx-auto
+            bg-white
+            px-10
+            sm:px-20
+            py-5
+            flex-none flex flex-wrap
+            border-t border-gray-200
+            mt-2
+            flex flex-row
+            justify-between
+        "
+    >
+        <div>Made with ❤ by <a class="text-blue-700" href="https://baoshuo.ren" target="_blank">Baoshuo</a></div>
+        <div>
+            Project licensed under
+            <a
+                class="text-blue-700"
+                href="https://github.com/sjzezsyxx/sjzezsyxx.github.io/blob/master/LICENSE"
+                target="_blank"
+                >AGPLv3</a
+            >.
+        </div>
+    </div>
+</template>
