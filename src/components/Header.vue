d65fd5b8105ee749750176a9e9e8080380fa7467
Ren Baoshuo <i@baoshuo.ren>
2021-08-31 22:36:21 +0800

    fix(src/components/Header.vue): fix width when using large screen

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,38 +1,24 @@
 <template>
-    <header
-        class="
-            sticky
-            top-0
-            z-40
-            lg:z-50
-            w-full
-            mx-auto
-            bg-white
-            flex-none flex-wrap flex
-            px-10
-            sm:px-20
-            py-5
-            border-b border-gray-200
-            mb-2
-        "
-    >
-        <a href="/" class="flex-none pl-4 flex items-center lg:border-b-0 text-lg font-bold">
-            {{ title }}
-        </a>
-        <div class="flex-auto h-18 flex items-center justify-between px-4 sm:px-6 lg:mx-6 lg:px-0 xl:mx-8">
-            <div class="flex items-center space-x-6"></div>
-            <div>
-                <a
-                    href="http://www.sjzezsyxx.com"
-                    class="text-gray-400 hover:text-gray-500 transition-colors duration-200 px-1 md:px-3"
-                >
-                    石家庄二中实验学校
-                </a>
-                <a
-                    href="https://github.com/sjzezsyxx"
-                    class="text-gray-400 hover:text-gray-500 transition-colors duration-200 px-1 md:px-3"
-                    >GitHub</a
-                >
+    <header class="sticky top-0 z-40 lg:z-50 w-full mx-auto bg-white px-10 sm:px-20 py-5 border-b border-gray-200 mb-2">
+        <div class="flex-none flex-wrap flex mx-auto max-w-screen-xl">
+            <a href="/" class="flex-none pl-4 flex items-center lg:border-b-0 text-lg font-bold">
+                {{ title }}
+            </a>
+            <div class="flex-auto h-18 flex items-center justify-between px-4 sm:px-6 lg:mx-6 lg:px-0 xl:mx-8">
+                <div class="flex items-center space-x-6"></div>
+                <div>
+                    <a
+                        href="http://www.sjzezsyxx.com"
+                        class="text-gray-400 hover:text-gray-500 transition-colors duration-200 px-1 md:px-3"
+                    >
+                        石家庄二中实验学校
+                    </a>
+                    <a
+                        href="https://github.com/sjzezsyxx"
+                        class="text-gray-400 hover:text-gray-500 transition-colors duration-200 px-1 md:px-3"
+                        >GitHub</a
+                    >
+                </div>
             </div>
         </div>
     </header>
