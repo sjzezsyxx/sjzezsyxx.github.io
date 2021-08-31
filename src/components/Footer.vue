cdd7030cf0508d47f053b53ddf1bec17c4450a3a
Ren Baoshuo <i@baoshuo.ren>
2021-08-31 22:38:42 +0800

    fix(src/components/Footer.vue): fix width when using large screen

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,31 +1,16 @@
 <template>
-    <div
-        class="
-            bottom-0
-            z-40
-            lg:z-50
-            w-full
-            mx-auto
-            bg-white
-            px-10
-            sm:px-20
-            py-5
-            flex-none flex flex-wrap
-            border-t border-gray-200
-            mt-2
-            flex flex-row
-            justify-between
-        "
-    >
-        <div>Made with ❤ by <a class="text-blue-700" href="https://baoshuo.ren" target="_blank">Baoshuo</a></div>
-        <div>
-            Project licensed under
-            <a
-                class="text-blue-700"
-                href="https://github.com/sjzezsyxx/sjzezsyxx.github.io/blob/master/LICENSE"
-                target="_blank"
-                >AGPLv3</a
-            >.
+    <div class="bottom-0 z-40 lg:z-50 w-full mx-auto bg-white px-10 sm:px-20 py-5 border-t border-gray-200 mt-2">
+        <div class="flex-none flex flex-wrap flex-row justify-between mx-auto max-w-screen-xl">
+            <div>Made with ❤ by <a class="text-blue-700" href="https://baoshuo.ren" target="_blank">Baoshuo</a></div>
+            <div>
+                Project licensed under
+                <a
+                    class="text-blue-700"
+                    href="https://github.com/sjzezsyxx/sjzezsyxx.github.io/blob/master/LICENSE"
+                    target="_blank"
+                    >AGPLv3</a
+                >.
+            </div>
         </div>
     </div>
 </template>
