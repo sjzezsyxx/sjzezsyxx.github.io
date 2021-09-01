f1b2c354d3dc7047f7d2fb812fc58df6c99f8a1e
Ren Baoshuo <i@baoshuo.ren>
2021-09-01 10:27:38 +0800

    feat: add pwa

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import 'tailwindcss/tailwind.css';
+import { registerSW } from 'virtual:pwa-register';
 
 createApp(App).mount('#app');
+registerSW();
