6509a371fce1379e1093cefd37055c3e44ce6f68
Ren Baoshuo <i@baoshuo.ren>
2021-09-02 19:25:51 +0800

    revert: f1b2c354d3dc7047f7d2fb812fc58df6c99f8a1e

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import 'tailwindcss/tailwind.css';
-import { registerSW } from 'virtual:pwa-register';
 
 createApp(App).mount('#app');
-registerSW();
+
+if (navigator.serviceWorker) {
+    window.addEventListener('load', (event) => {
+        navigator.serviceWorker.getRegistrations()
+            .then((registrations) => {
+                registrations.forEach(registration => registration.unregister());
+                console.log('Successfully unregistered all service workers.');
+            });
+    });
+}
